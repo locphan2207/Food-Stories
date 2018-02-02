feefeccde37eb1a455263307bf477d55d4830be2
locphan2207 <locphan2207@gmail.com>
2018-02-01 17:12:00 -0800

    handle text

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,9 +14,31 @@ User.create(username: 'loc', email: 'loc@gmail.com', password: 'chetcha')
     img_url: "http://static1.businessinsider.com/image/586d0877dd0895e1148b45f4/the-50-best-food-bloggers-to-follow-on-instagram-in-2017.jpg",
     difficulty: "easy",
     ingredients: "hamburger: 1 piece, salad: 3 piece, salt: 3 lbs, garlic: 2 glove, soy sauce: 100 ml, suger: 50 g",
-    text: "Roughly chop pecans. Add sugar, water, and pecans to a pot. Allow the mixture to slowly simmer on medium heat for approx. 5 min. or until it thickens. Set aside and allow to cool.",
     baking_min: 20 + rand(40),
     preparation_min: 10 + rand(50),
-    resting_min: rand(30)
+    resting_min: rand(30),
+    text: "Step 1/5 Remove bones, skin, and sinews from beef, season evenly with salt, and allow to rest for 4 hrs.600 g beef shank50 g sea salt cutting boardknife Remove bones, skin, and sinews from beef, season evenly with salt, and allow to rest for 4 hrs.
+
+Step 2/5
+For the stock add 2/5 of the water, cardamom, star anise, bay leaves, chilis, cloves, coriander seeds, nutmeg, pepper, fennel seeds, lemon zest, rice wine, soy sauce, soybean paste, and sugar to a pot with lid over medium-high heat and bring to a boil. In the meantime, add beef to a large pot and top with remaining cold water and bring to a boil.
+
+5 l water3 cardamom pods2 star anise2 bay leaves2 dried chili3 cloves½ tsp coriander seeds1 nutmeg1 tsp mixed pepper½ tsp fennel seeds150 ml dark rice wine7 tbsp light soy sauce100 ml soy sauce2 tbsp soybean paste50 g sugarlemon zest
+
+potfine gratercooking spoonpot with lid
+For the stock add 2/5 of the water, cardamom, star anise, bay leaves, chilis, cloves, coriander seeds, nutmeg, pepper, fennel seeds, lemon zest, rice wine, soy sauce, soybean paste, and sugar to a pot with lid over medium-high heat and bring to a boil. In the meantime, add beef to a large pot and top with remaining cold water and bring to a boil.
+Step 3/5
+Finley chop ginger and roughly chop leek. Transfer ginger, leek, and beef into the stock; if needed, add more water. Bring to a boil and simmer over medium-low heat for approx. 1.5 hrs. or until tender.  Allow to cool, then transfer to the fridge for approx. 12 hrs.50 g ginger3 leek
+Finley chop ginger and roughly chop leek. Transfer ginger, leek, and beef into the stock; if needed, add more water. Bring to a boil and simmer over medium-low heat for approx. 1.5 hrs. or until tender. Allow to cool, then transfer to the fridge for approx. 12 hrs.
+Step 4/5
+For the sauce, finely slice garlic, add to a bowl, and mix with rice vinegar, sesame oil, remaining light soy sauce, and beef stock and season with salt and pepper. Stir to combine.
+2 cloves garlic2 tsp rice vinegar1 tsp sesame oil1 tbsp light soy sauce2 tsp beef stock1 pinch salt1 pinch pepper
+bowlwhisk
+For the sauce, finely slice garlic, add to a bowl, and mix with rice vinegar, sesame oil, remaining light soy sauce, and beef stock and season with salt and pepper. Stir to combine.
+Step 5/5
+Cut cucumber into slices. Remove beef from pot, gently dry with a paper towel and cut into slices. Garnish with sauce, cucumber, chili, and fresh cilantro. Enjoy!
+1 cucumberfresh cilantro for servingchili for serving
+paper towel
+Cut cucumber into slices. Remove beef from pot, gently dry with a paper towel and cut into slices. Garnish with sauce, cucumber, chili, and fresh cilantro. Enjoy!
+    "
   )
 end
--- frontend/components/recipes/recipe_show.jsx
+++ frontend/components/recipes/recipe_show.jsx
@@ -8,13 +8,10 @@ class RecipeShow extends React.Component {
   }
 
   componentWillMount() {
-    console.log("will mount");
     this.props.fetchRecipe(this.props.match.params.recipeId);
   }
 
   componentDidUpdate() {
-    console.log("did update");
-    // console.log(nextProp.recipe);
     const {preparation_min, baking_min, resting_min} = this.props.recipe;
     // We need canvasLoaded to only render canvas ONCE after first load
     if (!this.state.canvasLoaded) {
@@ -35,14 +32,44 @@ class RecipeShow extends React.Component {
     }
   }
 
+  textHandling() {
+    let textCopy = this.props.recipe.text; //copy text
+    let textArray = [];
+    // find "Step" and save to title:
+    let startIndexOfStep = textCopy.indexOf('Step');
+
+    while (startIndexOfStep !== -1) {
+      // debugger
+      const endIndexOfStep = startIndexOfStep + 8;
+      const stepTitle = textCopy.slice(startIndexOfStep, endIndexOfStep); //save title
+      textCopy = textCopy.slice(endIndexOfStep, textCopy.length); //remove saved title
+
+      let stepBody;
+      startIndexOfStep = textCopy.indexOf('Step'); //search for next "Step"
+      if (startIndexOfStep < 0) {
+        stepBody = textCopy.slice(0, textCopy.length);
+      }
+      else {
+        stepBody = textCopy.slice(0, startIndexOfStep);
+      }
+
+      textArray.push( //save as react element
+        <div className="step">
+          <p className="sub-title">{stepTitle}</p>
+          <p>{stepBody}</p>
+        </div>
+      );
+    }
+    return textArray;
+  }
+
   render() {
-    console.log("rendering");
-    console.log(this.props);
     const {recipe} = this.props;
     //THIS IS WEIRD but it takes extra 1 react cycle to get params, so:
     if (!recipe || !recipe.ingredients) return (<div></div>);
 
     //Setup:
+
     let ingredientRows = recipe.ingredients.split(", "); //split by comma
     ingredientRows = ingredientRows.map(row => {
       //We split the pair by colon, and show eacg in table
@@ -56,6 +83,7 @@ class RecipeShow extends React.Component {
         </tr>
       );
     });
+
 
     //Rendering:
     return (
@@ -90,7 +118,7 @@ class RecipeShow extends React.Component {
               <span>Too few ratings</span>
             </p>
             <div className="difficulty">
-              <span className="dif-title">Difficulty</span>
+              <span className="sub-title">Difficulty</span>
               <span>{recipe.difficulty} 👌</span>
             </div>
             <div className="time">
@@ -111,7 +139,7 @@ class RecipeShow extends React.Component {
               </div>
             </div>
             <div className="ingredients">
-              <p className="dif-title">Ingredients</p>
+              <p className="sub-title">Ingredients</p>
               <p className="serving">Servings:
                 <button onClick={() => this.ingredientMultiply("minus")}>-</button>
                 {this.state.servingNum}
@@ -121,7 +149,9 @@ class RecipeShow extends React.Component {
                 {ingredientRows}
               </tbody></table>
             </div>
-            <p>{recipe.text}</p>
+            <div className="text">
+              {this.textHandling()}
+            </div>
           </div>
           <div className="right-col">
         </div>
