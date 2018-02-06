f3515c240973056c84cd6c3ee9b6e7c01818ec03
locphan2207 <locphan2207@gmail.com>
2018-02-06 14:32:56 -0800

    implement comment reply

--- frontend/components/comments/comment_index.jsx
+++ frontend/components/comments/comment_index.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 class CommentIndex extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {replyTargetId: undefined};
   }
 
   returnReplies(parentCommentId) {
@@ -22,10 +24,18 @@ class CommentIndex extends React.Component {
   returnTopLvComment() {
     const topLevelComments = [];
     this.props.comments.forEach(comment => {
+      let replyForm = null;
+      if (this.state.replyTargetId === comment.id) {
+        // pass the target id to let form know parent_comment_id
+        replyForm = this.generateCommentForm(comment.id);
+      }
       if (!comment.parent_comment_id) {
         topLevelComments.push(<div>
           <p>Author {comment.author_id}: {comment.body}</p>
-          <button>Reply</button>
+          <button onClick={e => this.setState({replyTargetId: comment.id})}>
+            Reply
+          </button>
+          {replyForm}
           <img src={comment.img_url}></img>
           {this.returnReplies(comment.id)}
         </div>);
@@ -34,11 +44,34 @@ class CommentIndex extends React.Component {
     return topLevelComments;
   }
 
-  submitComment(e) {
+  generateCommentForm(parentCommentId) {
+    let commentForm = (
+      <div>
+        <Link to="/signup">Sign Up</Link>
+        <p>or</p>
+        <Link to="/login">Log In</Link>
+        <p>To comment and share your experience</p>
+      </div>
+    );
+    // If dont pass parentCommentId arg, we can understand this is top LV comment
+    // If it has arg, it is a reply comment
+    if (this.props.currentUser) {
+      commentForm = (
+        <form onSubmit={e => this.submitComment(e, parentCommentId)}>
+          <input ref="commentInput" type="text"/>
+          <input type="submit" value="Send" />
+        </form>
+      );
+    }
+    return commentForm;
+  }
+
+  submitComment(e, parentCommentId) {
     e.preventDefault();
     const newComment = {
       author_id: this.props.currentUser.id,
       body: this.refs.commentInput.value,
+      parent_comment_id: parentCommentId
     };
     this.props.postComment(newComment);
   }
@@ -48,10 +81,7 @@ class CommentIndex extends React.Component {
 
     return (
       <div>
-        <form onSubmit={e => this.submitComment(e)}>
-          <input ref="commentInput" type="text"/>
-          <input type="submit" value="Send" />
-        </form>
+        {this.generateCommentForm(false)}
         <ul>
           {
             this.returnTopLvComment()
