6f6f1f7923f0fa70ff94bc6dca58ec4f94538a09
locphan2207 <locphan2207@gmail.com>
2018-02-05 17:21:28 -0800

    implemement active record

--- frontend/components/recipes/recipe_search.jsx
+++ frontend/components/recipes/recipe_search.jsx
@@ -6,6 +6,27 @@ class RecipeSearch extends React.Component {
     this.state = {title: "", difficulty: "", maxCookingTime: ""};
   }
 
+  generateActiveFilters() {
+    if (this.state.difficulty === "" && this.state.maxCookingTime === "") {
+      return null;
+    }
+    let difficultyFilter = null;
+    let timeFilter = null;
+    if (this.state.difficulty !== "") {
+      difficultyFilter = (<p onClick={e => this.setState({difficulty: ""})}>
+      {this.state.difficulty}</p>);
+    }
+    if (this.state.maxCookingTime !== "") {
+      timeFilter = (<p onClick={e => this.setState({maxCookingTime: ""})}>
+      {this.state.maxCookingTime}</p>);
+    }
+
+    return (
+      <div>Active Filters: (click on it to remove filter):
+        {[difficultyFilter, timeFilter]}</div>
+    );
+  }
+
   handleChange(e, property) {
     this.setState({[property]: e.target.value});
   }
@@ -19,25 +40,28 @@ class RecipeSearch extends React.Component {
   render() {
     console.log(this.state);
     return (
-      <form onSubmit={e => this.handleSubmit(e)}>
-        <input onChange={e => this.handleChange(e, 'title')}
-          type="text" name="title" value={this.state.title}/>
-        <select value={this.state.difficulty}
-            onChange={e => this.handleChange(e, 'difficulty')}>
-          <option value="" disabled hidden>Difficulty</option>
-          <option value="easy">Easy</option>
-          <option value="medium">Medium</option>
-          <option value="hard">Hard</option>
-        </select>
-        <select value={this.state.maxCookingTime}
-            onChange={e => this.handleChange(e, 'maxCookingTime')}>
-          <option value="" disabled hidden>Cooking Time</option>
-          <option value="20">Under 20</option>
-          <option value="40">Under 40</option>
-          <option value="60">Under 60</option>
-        </select>
-        <input type="submit" value="Search" />
-      </form>
+      <div>
+        <form onSubmit={e => this.handleSubmit(e)}>
+          <input onChange={e => this.handleChange(e, 'title')}
+            type="text" name="title" value={this.state.title}/>
+          <select value={this.state.difficulty}
+              onChange={e => this.handleChange(e, 'difficulty')}>
+            <option value="" disabled>Difficulty</option>
+            <option value="easy">Easy</option>
+            <option value="medium">Medium</option>
+            <option value="hard">Hard</option>
+          </select>
+          <select value={this.state.maxCookingTime}
+              onChange={e => this.handleChange(e, 'maxCookingTime')}>
+            <option value="" disabled>Cooking Time</option>
+            <option value="20">Under 20</option>
+            <option value="40">Under 40</option>
+            <option value="60">Under 60</option>
+          </select>
+          <input type="submit" value="Search" />
+        </form>
+        {this.generateActiveFilters()}
+      </div>
     );
   }
 }
