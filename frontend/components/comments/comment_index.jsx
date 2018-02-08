4ccc5651c6ac7e99e4391b0808a89b878e43cceb
locphan2207 <locphan2207@gmail.com>
2018-02-07 18:36:56 -0800

    fix heart with pic

--- frontend/components/comments/comment_index.jsx
+++ frontend/components/comments/comment_index.jsx
@@ -26,12 +26,12 @@ class CommentIndex extends React.Component {
                 <p>{comment.body}</p>
               </div>
             </div>
+            {this.generatePic(comment.img_url)}
             <div className="reply-comment-like-area">
               <img className="like" src={this.props.generateLike(comment)}
                 onClick={e => this.props.toggleLike("comments", comment, e)}/>
               <div className="like-count">{comment.likeIds.length}</div>
             </div>
-            {this.generatePic(comment.img_url)}
           </div>
         );
       }
