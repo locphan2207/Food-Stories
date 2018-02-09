58aca90e8ea5254ddd114fb29ab2db876d2bcdae
locphan2207 <locphan2207@gmail.com>
2018-02-09 15:35:43 -0800

    test

--- db/seeds.rb
+++ db/seeds.rb
@@ -63,7 +63,7 @@ recipe_name = [
 ]
 (1..20).each do |i|
   Recipe.create(
-    title: recipe_name[i-1],
+    title: "Testing recipe #{i}",
     author_id: "1",
     img_url: img_url[i-1],
     difficulty: difficulty[rand(2)],
